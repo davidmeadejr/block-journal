8b4a678874f128069d40ce11be78223f0c45bf1b
David Meade Jr <davidmeadejnr@gmail.com>
2022-10-15 16:40:55 +0100

    Feat: WAGMI solidity contract created.

---
+++ contracts/wagmi.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.17;
+
+import "hardhat/console.sol";
+
+contract WAGMI {
+    constructor() {
+        console.log("Contructor here, reporting for duty, WAGMI.");
+    }
+}
