f204b32916c95224bd6522d2e6f7b40d283d831b
David Meade Jr <davidmeadejnr@gmail.com>
2022-10-19 18:42:20 +0100

    Feat: Log journal and get total journals functionality added.

--- contracts/BlockJournal.sol
+++ contracts/BlockJournal.sol
@@ -5,7 +5,19 @@ pragma solidity ^0.8.17;
 import "hardhat/console.sol";
 
 contract BlockJournal {
+    uint256 totalJournals;
+
     constructor() {
         console.log("Contructor here, reporting for duty, WAGMI.");
+    }
+
+    function journal() public {
+        totalJournals += 1;
+        console.log("%s has journalled!", msg.sender);
+    }
+
+    function getTotalJournals() public view returns (uint256) {
+        console.log("%d total journals!", totalJournals);
+        return totalJournals;
     }
 }
--- scripts/run.js
+++ scripts/run.js
@@ -1,8 +1,23 @@
 const main = async () => {
+  const [owner, randomPerson] = await hre.ethers.getSigners(); // Gets the wallet address of the contract owner and a random wallet address for testing purposes.
   const journalContractFactory = await hre.ethers.getContractFactory("BlockJournal"); // Compiles the contract and generates all the needed files to use the contract under the artifacts directory.
   const journalContract = await journalContractFactory.deploy(); //
   await journalContract.deployed();
+
   console.log("Contract deployed to:", journalContract.address);
+  console.log("Contract deployed by:", owner.address);
+
+  await journalContract.getTotalJournals(); // Grabs the total number of journals.
+
+  const firstJournalTxn = await journalContract.journal();
+  await firstJournalTxn.wait();
+
+  await journalContract.getTotalJournals();
+
+  const secondJournalTxn = await journalContract.connect(randomPerson).wave();
+  await secondJournalTxn.wait();
+
+  await journalContract.getTotalJournals();
 };
 
 const runMain = async () => {
